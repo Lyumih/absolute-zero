b8fac9b1497e2e14fbc90ae6e5a0460615663bf8
Lyumih <m.ivanov1225@gmail.com>
2025-03-02 23:28:48 +0300

    fix log

--- mpds/nlp/nlp.ts
+++ mpds/nlp/nlp.ts
@@ -7,7 +7,6 @@ namespace $ {
             return $mol_import.script('https://unpkg.com/optimade-mpds-nlp@0.1.7/index.js').OptimadeNLP()
 		}
 		
-		@$log
 		static guess( text: string ) {
 			return this.lib().guess( text )
 		}
